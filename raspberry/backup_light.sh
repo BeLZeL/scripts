47d9d1f7ce473cb2e65d6213acf2340d04eed352
BeLZeL <belzel@gmail.com>
2017-11-09 20:58:19 +0100

    Create backup_light.sh

---
+++ raspberry/backup_light.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# ./backup_light.sh [ DEFAULT | MEDIAWIKI ]
+source ~/check_remote_dir.sh
+
+#-------------------------------------------------------------------------------
+
+LIGHT_DIR="${REMOTE_DIR}/light"
+if [ ! -d ${LIGHT_DIR} ]
+then
+    mkdir ${LIGHT_DIR}
+fi
+
+#-------------------------------------------------------------------------------
+
+# Generate crontab
+crontab -l >  ${LIGHT_DIR}/crontab
+
+# Generate dpkg
+dpkg -l > ${LIGHT_DIR}/dpkg.txt
+
+# Copy home (-a = -rlptgoD, remove -g and -o)
+rsync -rlptDvz --exclude "*.log" --delete ~/ ${LIGHT_DIR}/home
+
+# Copy etc
+rsync -rlptDvz \
+	/etc/fstab \
+	/etc/apt/sources.list \
+	/etc/apt/sources.list.d/raspi.list \
+	${LIGHT_DIR}/etc
+
+#-------------------------------------------------------------------------------
+
+case ${MODE} in
+
+DEFAULT)
+    sudo tar -hcvJf ${REMOTE_DIR}/backup_light_${MODE}_$(date +"%Y%m%d").txz \
+        ${LIGHT_DIR}/*
+    ;;
+
+MEDIAWIKI)
+    mysqldump --user=root --password="${MYSQL_PASSWD}" wikidb > ${LIGHT_DIR}/wikidb.sql
+    sudo tar -hcvJf ${REMOTE_DIR}/backup_light_${MODE}_$(date +"%Y%m%d").tar.xz \
+        ${LIGHT_DIR}/* \
+        /etc/mediawiki/mediawiki.conf \
+        /etc/mediawiki/LocalSettings.php \
+        /etc/apache2/apache2.conf \
+		/var/lib/mediawiki/images \
+		/var/lib/mediawiki/extensions
+    ;;
+
+
+
+*)
+    date +"%Y%m%d %H%M%S $0 : Error with MODE=${MODE} and REMOTE_DIR=${REMOTE_DIR} and LIGHT_DIR=${LIGHT_DIR}: Unknown MODE" >> ~/backup.log
+    exit 1
+    ;;
+
+esac
+
+#-------------------------------------------------------------------------------
+
+date +"%Y%m%d %H%M%S $0 : End with MODE=${MODE} and REMOTE_DIR=${REMOTE_DIR}" >> ~/backup.log
+
+#-------------------------------------------------------------------------------
