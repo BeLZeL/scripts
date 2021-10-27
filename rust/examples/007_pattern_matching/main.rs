8831f8ff343f7fb64d6bd92b838d57eecc9695f6
BeLZeL <belzel@gmail.com>
2021-10-27 10:57:46 +0200

    rust : 007_pattern_matching : add some new patterns

--- rust/examples/007_pattern_matching/main.rs
+++ rust/examples/007_pattern_matching/main.rs
@@ -2,7 +2,7 @@ fn main() {
 
     for i in 1..=101 {
         match i {
-            x @ 50 ... 100 => println!("{} est entre 50 et 100", x),
+            x @ 50 ..= 100 => println!("{} est entre 50 et 100", x),
             11 | 12 => println!("{} est égal à 11 ou 12", i),
             _ => println!("{} n'est ni 11, ni 12, ni entre 50 et 100", i),
         }
@@ -19,4 +19,30 @@ fn main() {
             _ => println!("{}", n)
         }
     }
+    
+    
+    // https://stackoverflow.com/questions/25383488/how-to-match-a-string-against-string-literals
+    let stringthing = String::from("c");
+    
+    match &stringthing[..] {
+        "a" => println!("0"),
+        "b" => println!("1"),
+        "c" => println!("2"), // <-------
+        _ => println!("something else!"),
+    }
+
+    match stringthing.as_str() {
+        "a" => println!("0"),
+        "b" => println!("1"),
+        "c" => println!("2"), // <-------
+        _ => println!("something else!"),
+    }
+
+    match stringthing.as_ref() {
+        "a" => println!("0"),
+        "b" => println!("1"),
+        "c" => println!("2"), // <-------
+        _ => println!("something else!"),
+    }
+
 }
