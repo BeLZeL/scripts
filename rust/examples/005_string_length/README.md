ebbd5eedf0968fb505bd8d646e0e868a5e30891b
BeLZeL <belzel@gmail.com>
2024-01-23 16:27:39 +0100

    Rust : 005_string : README + improves examples

---
+++ rust/examples/005_string_length/README.md
@@ -0,0 +1,8 @@
+String avec Cargo
+
+En résumé :
+* Utilisation de `for` avec `chars` pour parcourir une chaîne de caractères
+* Mise en place de slice via référence
+
+Liens :
+* https://www.programiz.com/rust/string
--- rust/examples/005_string_length/src/main.rs
+++ rust/examples/005_string_length/src/main.rs
@@ -1,29 +1,26 @@
 /* Read : & */
-fn calculate_length(s: & String) -> usize {
-
+fn calculate_length(s: &String) -> usize {
     s.len()
-
 }
-
 
 /* Write : &mut */
 fn change(s: &mut String) {
-
     s.push_str(" World !")
-
 }
 
 fn main() {
-
     let mut s1 = String::from("hello");
+    for char in s1.chars() {
+        println!("{}", char); // show h e l l o
+    }
 
     let mut len = calculate_length(&s1);
-
-    println!("la longueur est {}", len);
+    println!("la longueur est {}", len); // show 5
 
     change(&mut s1);
-    
     len = calculate_length(&s1);
-    
-    println!("la longueur est {}", len);
+    println!("la longueur est {}", len); // show 13
+
+    let slice = &s1[6..];
+    println!("la fin est '{}'", slice); // show "World !"
 }
